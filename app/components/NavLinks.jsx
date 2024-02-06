6ecb96b995bd82467cc191b6fdf9ee77b73346cf
Fernando <fernan.sax@hotmail.com>
2024-02-06 20:47:32 +0100

    scroll

--- app/components/NavLinks.jsx
+++ app/components/NavLinks.jsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 
 function NavLinks({href , title  }) {
   return (
-     <Link href = {href} className = "block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounded md:p-0 hover:text-white">
+     <a href = {href} className = "block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounded md:p-0 hover:text-white">
         {title}
-     </Link>
+     </a>
   )
 }
 
